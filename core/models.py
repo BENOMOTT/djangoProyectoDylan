b9acce50c5398ece66d30bbf323211aec1be449d
BENOMOTT <dylan.r.ubilla@gmail.com>
2023-05-27 12:49:06 -0400

    segundo commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria (models.model):
+    idCategoria = models.IntegerField(primary_key=True,
+    verbose_name='id de categoria')
+    nombreCategoria = models.CharField(max_length=50,
+    verbose_name='nombre de la categoria')
+
+    def _str_(self):
+        return self.nombreCategoria
